6b41922aace4ba2b9442dd8b9abb7756e243b5ba
shy060817 <alex.wanjun@gmail.com>
2019-09-14 22:44:37 +0900

    Updates

---
+++ precache-manifest.333be45771266e85d1ce244fda2b0231.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459467b0c379751a6833434ada73d075",
+    "url": "/nomflix/index.html"
+  },
+  {
+    "revision": "dec4a9fc6140190d0ef7",
+    "url": "/nomflix/static/js/2.117452ff.chunk.js"
+  },
+  {
+    "revision": "d590ced79af7a9325c98",
+    "url": "/nomflix/static/js/main.553cb015.chunk.js"
+  },
+  {
+    "revision": "a5c4e370f881ffb2ccde",
+    "url": "/nomflix/static/js/runtime~main.963d735e.js"
+  },
+  {
+    "revision": "a8bc8049e3940004f4c3ab515d3eadea",
+    "url": "/nomflix/static/media/noPosterSmall.a8bc8049.png"
+  }
+]);
